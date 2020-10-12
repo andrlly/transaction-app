09d30820737d9c3fd790eed7a30ede89bfac044f
andrlly <andriy.venglinskiy@gmail.com>
2020-10-12 21:31:29 +0300

    for deploy

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static(__dirname + '/dist/transaction-app'))
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/dist/transaction-app/index.html'))
+})
+
+app.listen(process.env.PORT || 8080);
