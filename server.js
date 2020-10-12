269914454b466814926c878599eb6ebcb606c417
andrlly <andriy.venglinskiy@gmail.com>
2020-10-12 21:53:40 +0300

    change server

--- server.js
+++ server.js
@@ -1,23 +1,23 @@
-// const express = require('express');
-// const path = require('path');
-//
-// const app = express();
-//
-// app.use(express.static(__dirname + '/dist/transaction-app'))
-//
-// app.get('*', (req, res) => {
-//   res.sendFile(path.join(__dirname + '/dist/transaction-app/index.html'))
-// })
-//
-// app.listen(process.env.PORT || 8080);
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static(__dirname + '/dist/transaction-app'))
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/dist/transaction-app/index.html'))
+})
 
 const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('transaction.json');
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 3000;
 
 server.use(middlewares);
 server.use(router);
 
-server.listen(port);
+app.listen(process.env.PORT || 8080);
+
+
+
