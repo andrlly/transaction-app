11e3b5f490d45d58634142765dc8767855ded3d7
andrlly <andriy.venglinskiy@gmail.com>
2020-10-13 09:48:30 +0300

    change server

--- src/app/transaction/services/transactions-api.service.ts
+++ src/app/transaction/services/transactions-api.service.ts
@@ -4,7 +4,7 @@ import { Observable } from 'rxjs';
 import { ITransaction, ITransactionRequest } from '@transactionModule/interfaces/transaction.interface';
 import { map } from 'rxjs/operators';
 import { Mappers } from '@sharedModule/utils/mappers';
-import { environment } from '@environment/environment.prod';
+import { environment } from '@environment/environment';
 
 @Injectable({
   providedIn: 'root'
